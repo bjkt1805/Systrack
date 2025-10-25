e6f8d19c354b7ed9dd2a4eb2b9a73bd8d1a81dc1
warnersalazarbadilla <wasalazarba@est.utn.ac.cr>
2025-10-25 16:58:20 -0600

    Update valoracionServicio seed data to correct ticketId and creadoPorId values

--- server/prisma/seeds/valoracionServicio.ts
+++ server/prisma/seeds/valoracionServicio.ts
@@ -1,8 +1,14 @@
 export const valoracionServicio = [
   {
-    ticketId: 4,
+    ticketId: 3,
     comentario: "Servicio excepcional, superó mis expectativas.",
     puntaje: 5,
-    creadoPorId: 1,
+    creadoPorId: 11,
+  },
+  {
+    ticketId: 7,
+    comentario: "Muy buen servicio, pero hubo un pequeño retraso.",
+    puntaje: 4,
+    creadoPorId: 15,
   }
 ];
